c485b6c8046b8336abd5a035b18e4e85ebea6e4d
Paul Van Hecke <p.vanhecke86@gmail.com>
2017-07-07 03:12:47 +0200

    Connexion + gestion avatar : ok Manque rappel mdp

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,13 +4,57 @@ class User < ApplicationRecord
   has_secure_password
   has_secure_token :confirmation_token
 
+  after_save :avatar_after_upload
+  after_destroy :avatar_destroy
+  before_save :avatar_before_upload
+
+  validates :avatar_file, file: {ext:[:jpg, :png]}
+
   validates :username, format: {with: /\A[a-zA-Z0-9_]{2,20}\z/, message: 'ne doit contenir que des caractères alphanumérique ou des underscores'},
   uniqueness: {case_sensitive: false}
   validates :email, format:{with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/, message: 'invalide'},
   uniqueness: {case_sensitive: false}
-  validates :avatar_file, file: {ext:[:jpg, :png]}
+
 
   def to_session
     {id: id}
+  end
+
+  def avatar_destroy
+    dir = File.direname(avatar_path)
+    FileUtils.rm_r(dir) if Dir.exist?(dir)
+
+  end
+
+  def avatar_url
+    '/' + [
+      self.class.name.downcase.pluralize,
+      id.to_s,
+      'avatar.jpg'
+    ].join('/')
+  end
+
+  def avatar_before_upload
+    if avatar_file.respond_to? :path
+      self.avatar = true
+    end
+  end
+
+  def avatar_path
+    File.join(
+    Rails.public_path,
+    self.class.name.downcase.pluralize,
+    id.to_s,
+    'avatar.jpg'
+    )
+  end
+
+  def avatar_after_upload
+    path = avatar_path
+    if avatar_file.respond_to? :path
+      dir = File.dirname(path)
+      FileUtils.mkdir_p(dir) unless Dir.exist?(dir)
+      FileUtils.cp(avatar_file.path, path)
+    end
   end
 end
---
+++ app/validators/file_validator.rb
@@ -0,0 +1,12 @@
+class FileValidator < ActiveModel::EachValidator
+  def validate_each(record,attribute,value)
+    if value.respond_to? :path
+      unless options[:ext].include? File.extname(value.path).delete('.').to_sym
+        record.errors[attribute] << "is not a valid file "
+      end
+    else
+      record.errors[attribute] << (options[:message]) || 'is not a file'
+
+    end
+  end
+end
